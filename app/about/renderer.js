e16efc8727bb0e68926f0f107979d45f24dd005d
SavageCore <talk@savagecore.eu>
2016-09-24 19:59:32 +0100

    Add version number to about page bottom bar

--- app/about/renderer.js
+++ app/about/renderer.js
@@ -1,2 +1,8 @@
 window.$ = window.jQuery = require('jquery');
 require('../menu')
+require('pkginfo')(module, 'version');
+
+$(document).ready(function() {
+    $('.bottom-bar').addClass('bottom-bar-info')
+    $('.bottom-bar').html(`Version: ${module.exports.version}`)
+});
