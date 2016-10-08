af430c03ae0d17804c0b116ec57ab84d02e5f802
SavageCore <talk@savagecore.eu>
2016-10-08 04:17:22 +0100

    Switch to XO Linter

--- app/menu.js
+++ app/menu.js
@@ -1,50 +1,51 @@
-const electron = require('electron')
-const remote = electron.remote
-const mainProcess = remote.require('./main')
-const {
-    ipcRenderer
-} = require('electron')
+const electron = require('electron');
 
-$(document).ready(function() {
-    var isMenuOpen = false;
+const remote = electron.remote;
+const mainProcess = remote.require('./main');
 
-    $('.menu_btn').click(function() {
-        if (isMenuOpen === false) {
-            $("#menu_smartphone").clearQueue().animate({
-                right: '0px'
-            })
-            $("#grey_back").fadeIn('fast');
+/* global $:true */
+/* global document:true */
 
-            isMenuOpen = true;
-        }
-    });
-    $('#grey_back').click(function() {
-        if (isMenuOpen === true) {
-            $("#menu_smartphone").clearQueue().animate({
-                right: '-570px'
-            })
-            $("#page").clearQueue().animate({
-                "margin-left": '0px'
-            })
-            $("#grey_back").fadeOut('fast');
+$(document).ready(function () {
+	var isMenuOpen = false;
 
-            isMenuOpen = false;
-        }
-    });
+	$('.menu_btn').click(function () {
+		if (isMenuOpen === false) {
+			$('#menu_smartphone').clearQueue().animate({
+				right: '0px'
+			});
+			$('#grey_back').fadeIn('fast');
+
+			isMenuOpen = true;
+		}
+	});
+	$('#grey_back').click(function () {
+		if (isMenuOpen === true) {
+			$('#menu_smartphone').clearQueue().animate({
+				right: '-570px'
+			});
+			$('#page').clearQueue().animate({
+				'margin-left': '0px'
+			});
+			$('#grey_back').fadeOut('fast');
+
+			isMenuOpen = false;
+		}
+	});
 });
 
-$("#menu_about").click(function() {
-    mainProcess.switchPage('about')
+$('#menu_about').click(function () {
+	mainProcess.switchPage('about');
 });
 
-$("#menu_main").click(function() {
-    mainProcess.switchPage('main')
+$('#menu_main').click(function () {
+	mainProcess.switchPage('main');
 });
 
-$("#menu_settings").click(function() {
-    mainProcess.switchPage('settings')
+$('#menu_settings').click(function () {
+	mainProcess.switchPage('settings');
 });
 
-$("#menu_quit").click(function() {
-    mainProcess.quitApp()
+$('#menu_quit').click(function () {
+	mainProcess.quitApp();
 });
