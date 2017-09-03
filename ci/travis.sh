208b03ef9441c126ea40bfc2bb0d8a2f579765c2
SavageCore <talk@savagecore.eu>
2017-09-03 08:59:16 +0100

    Use current year for electron-packager

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
-	electron-packager "$TRAVIS_BUILD_DIR" "Attract Screen Tool" --platform=linux --arch=x64 --app-version=1.0.$TRAVIS_BUILD_NUMBER --electron-version=1.7.5 --out="$TRAVIS_BUILD_DIR/builds" --overwrite --app-copyright="Copyright (C) 2017 SavageCore"
+	electron-packager "$TRAVIS_BUILD_DIR" "Attract Screen Tool" --platform=linux --arch=x64 --app-version=1.0.$TRAVIS_BUILD_NUMBER --electron-version=1.7.5 --out="$TRAVIS_BUILD_DIR/builds" --overwrite --app-copyright="Copyright (C) $(date +'%Y') SavageCore"
 	7z a Attract-Screen-Tool-linux-x64-$TRAVIS_COMMIT.zip "$TRAVIS_BUILD_DIR/builds/Attract Screen Tool-linux-x64"
 	sftp $SFTP_USER@savagecore.eu:artifacts/ <<< $'put Attract-Screen-Tool-linux-x64-'$TRAVIS_COMMIT'.zip'
 fi
 if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
-	electron-packager "$TRAVIS_BUILD_DIR" "Attract Screen Tool" --platform=darwin --arch=x64 --app-version=1.0.$TRAVIS_BUILD_NUMBER --electron-version=1.7.5 --out="$TRAVIS_BUILD_DIR/builds" --overwrite --app-copyright="Copyright (C) 2017 SavageCore"
+	electron-packager "$TRAVIS_BUILD_DIR" "Attract Screen Tool" --platform=darwin --arch=x64 --app-version=1.0.$TRAVIS_BUILD_NUMBER --electron-version=1.7.5 --out="$TRAVIS_BUILD_DIR/builds" --overwrite --app-copyright="Copyright (C) $(date +'%Y') SavageCore"
 	7z a Attract-Screen-Tool-darwin-x64-$TRAVIS_COMMIT.zip "$TRAVIS_BUILD_DIR/builds/Attract Screen Tool-darwin-x64"
 	sftp $SFTP_USER@savagecore.eu:artifacts/ <<< $'put Attract-Screen-Tool-darwin-x64-'$TRAVIS_COMMIT'.zip'
 fi
